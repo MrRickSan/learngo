a78a8925fc701e7e9645242875535bf64a7f7aa2
Ricardo Oliveira <ricardo.oliveira@mindera.com>
2019-02-28 00:45:09 +0000

    variable exercises

---
+++ first/exercises/06-variables/02-declarations/04-string/main.go
@@ -0,0 +1,29 @@
+// For more tutorials: https://blog.learngoprogramming.com
+//
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+
+package main
+
+import "fmt"
+
+// ---------------------------------------------------------
+// EXERCISE: Declare string
+//
+//  1. Declare a string variable
+//
+//  2. Print that variable
+//
+// EXPECTED OUTPUT
+//  ""
+// ---------------------------------------------------------
+
+func main() {
+	var s string
+	fmt.Printf("s (%T): %q\n", s, s)
+
+	// %T prints the type of the value
+	// %q prints an empty string
+}
