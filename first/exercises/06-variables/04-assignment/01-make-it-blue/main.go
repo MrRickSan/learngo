aaf28ddaa08a4199fc1704e593d6cd6fa1c9003b
Ricardo Oliveira <ricardo.oliveira@mindera.com>
2019-03-04 00:44:01 +0000

    more exercises

---
+++ first/exercises/06-variables/04-assignment/01-make-it-blue/main.go
@@ -0,0 +1,29 @@
+// For more tutorials: https://blog.learngoprogramming.com
+//
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+
+package main
+
+import "fmt"
+
+// ---------------------------------------------------------
+// EXERCISE: Make It Blue
+//
+//  1. Change `color` variable's value to "blue"
+//
+//  2. Print it
+//
+// EXPECTED OUTPUT
+//  blue
+// ---------------------------------------------------------
+
+func main() {
+	color := "green"
+
+	color = "blue"
+
+	fmt.Println(color)
+}
