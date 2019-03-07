c457f60ed8e2fb74b44e145933fb8f8d5346c8b1
Ricardo Oliveira <ricardo.oliveira@mindera.com>
2019-03-07 18:04:26 +0000

    printing types

--- 07-printf/03-printing-types/main.go
+++ 07-printf/03-printing-types/main.go
@@ -11,12 +11,19 @@ func main() {
 	)
 
 	fmt.Printf("Planet variable type: %T\n", planet)
-	fmt.Printf("Planet: %v\n", planet)
-	fmt.Printf("Distance: %v millions of kms\n", distance)
-	fmt.Printf("Orbital Period: %v days\n", orbital)
-	fmt.Printf("Does %v have life? %v\n", planet, hasLife)
+	fmt.Printf("Planet: %s\n", planet)                     // %s = string
+	fmt.Printf("Distance: %d millions of kms\n", distance) // %d = decimal
+	fmt.Printf("Orbital Period: %f days\n", orbital)       // %f = float
+	fmt.Printf("Does %s have life? %t\n", planet, hasLife) // %t = bool
 
 	// Argument Index (in printf indexes start at 1!)
 	fmt.Printf("%v is %v away. Think! %[2]v kms! %[1]v OMG!\n",
 		planet, distance)
+
+	fmt.Println()
+	fmt.Printf("Orbital Period: %.0f days\n", orbital)
+	fmt.Printf("Orbital Period: %.1f days\n", orbital)
+	fmt.Printf("Orbital Period: %.2f days\n", orbital)
+	fmt.Printf("Orbital Period: %.3f days\n", orbital)
+	fmt.Printf("Orbital Period: %.4f days\n", orbital)
 }
