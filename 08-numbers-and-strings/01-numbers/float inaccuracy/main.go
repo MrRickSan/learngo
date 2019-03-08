988c525f3aa749ad5cecf7fe65073463f698c9d0
Ricardo Oliveira <ricardo.oliveira@mindera.com>
2019-03-08 16:54:48 +0000

    float inaccuracy

---
+++ 08-numbers-and-strings/01-numbers/float inaccuracy/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	ratio := 1.0 / 10
+	fmt.Printf("%.60f\n", ratio)
+
+	// output
+	// 0.100000000000000005551115123125782702118158340454101562500000
+}
