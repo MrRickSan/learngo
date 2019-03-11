003bde7fdd3b710fe6f47cc376879f5ad611e22e
Ricardo Oliveira <ricardo.oliveira@mindera.com>
2019-03-11 16:46:40 +0000

    numbers, precedence, and assignment operations exercises

---
+++ 08-numbers-and-strings/01-numbers/exercises/06-simplify-the-assignments/main.go
@@ -0,0 +1,38 @@
+// For more tutorials: https://blog.learngoprogramming.com
+//
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+
+package main
+
+import "fmt"
+
+// ---------------------------------------------------------
+// EXERCISE: Simplify the Assignments
+//
+//  Simplify the code (refactor)
+//
+// RESTRICTION
+//  Use only the incdec and assignment operations
+//
+// EXPECTED OUTPUT
+//  3
+// ---------------------------------------------------------
+
+func main() {
+	width, height := 10, 2
+
+	width++
+	width += height
+
+	width--
+	width -= height
+
+	width *= 20
+	width /= 25
+	width %= 5
+
+	fmt.Println(width)
+}
