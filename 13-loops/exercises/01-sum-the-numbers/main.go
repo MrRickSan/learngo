9172cc994f2529733761f88c8e002ccf96109c01
Ricardo Oliveira <ricardo.oliveira@mindera.com>
2019-07-23 18:00:20 +0100

    loops exercise

---
+++ 13-loops/exercises/01-sum-the-numbers/main.go
@@ -0,0 +1,29 @@
+// For more tutorials: https://blog.learngoprogramming.com
+//
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+
+package main
+
+import "fmt"
+
+// ---------------------------------------------------------
+// EXERCISE: Sum the Numbers
+//
+//  1. By using a loop, sum the numbers between 1 and 10.
+//  2. Print the sum.
+//
+// EXPECTED OUTPUT
+//  Sum: 55
+// ---------------------------------------------------------
+
+func main() {
+	var sum int
+	for i := 1; i <= 10; i++ {
+		sum += i
+	}
+
+	fmt.Printf("Sum: %d\n", sum)
+}
