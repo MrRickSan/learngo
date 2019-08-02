b4ca11a4f32e496167e0451202225208a7a81bcf
Ricardo Oliveira <ricardo.oliveira@mindera.com>
2019-08-02 15:09:40 +0100

    finish array exercises

---
+++ 14-arrays/exercises/12-sorter/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+// ---------------------------------------------------------
+// EXERCISE: Number Sorter
+//
+//  Your goal is to sort the given numbers from the command-line.
+//
+//  1. Get the numbers from the command-line.
+//
+//  2. Create an array and assign the given numbers to that array.
+//
+//  3. Sort the given numbers and print them.
+//
+// RESTRICTION
+//   + Maximum 5 numbers can be provided
+//   + If one of the arguments are not a valid number, skip it
+//
+// HINTS
+//  + You can use the bubble-sort algorithm to sort the numbers.
+//    Please watch this: https://youtu.be/nmhjrI-aW5o?t=7
+//
+//  + When swapping the elements, do not check for the last element.
+//
+//    Or, you will receive this error:
+//    "panic: runtime error: index out of range"
+//
+// EXPECTED OUTPUT
+//   go run main.go
+//     Please give me up to 5 numbers.
+//
+//   go run main.go 6 5 4 3 2 1
+//     Sorry. Go arrays are fixed. So, for now, I'm only supporting sorting 5 numbers...
+//
+//   go run main.go 5 4 3 2 1
+//     [1 2 3 4 5]
+//
+//   go run main.go 5 4 a c 1
+//     [0 0 1 4 5]
+// ---------------------------------------------------------
+
+func main() {
+	args := os.Args[1:]
+
+	switch l := len(args); {
+	case l == 0:
+		fmt.Println("Please give me up to 5 numbers.")
+		return
+	case l > 5:
+		fmt.Println("Sorry. Go arrays are fixed. So, for now, I'm only supporting sorting 5 numbers...")
+		return
+	}
+
+	var numbers [5]int
+	for i, n := range args {
+		num, err := strconv.Atoi(n)
+		if err != nil {
+			continue
+		}
+
+		numbers[i] = num
+	}
+
+	swapped := true
+	for swapped {
+
+		swapped = false
+
+		for i := 0; i < len(numbers)-1; i++ {
+			if numbers[i] > numbers[i+1] {
+				numbers[i], numbers[i+1] = numbers[i+1], numbers[i]
+				swapped = true
+			}
+		}
+	}
+
+	fmt.Println(numbers)
+
+}
