8cf1e58016fc5e19fa0e9ac978dabf8fb0aadce6
Ricardo Oliveira <ricardo.oliveira@mindera.com>
2019-12-13 15:14:05 +0000

    include 100 to the loop

--- fizzBuzz.go
+++ fizzBuzz.go
@@ -4,14 +4,14 @@ import "fmt"
 
 func main() {
 
-	for i := 1; i < 100; i++ {
+	for i := 1; i <= 100; i++ {
 		var output string
 
-		if i%3 == 0 {
+		if i%5 == 0 {
 			output += "Fizz"
 		}
 
-		if i%5 == 0 {
+		if i%7 == 0 {
 			output += "Buzz"
 		}
 
