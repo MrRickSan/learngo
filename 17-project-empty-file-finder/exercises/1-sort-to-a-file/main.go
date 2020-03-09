0a705be4eec6dadfb9f57a0e685eae31b448db71
MrRicksan <ricardooliveira.info@gmail.com>
2020-03-09 01:32:33 +0000

    file finder exercises

--- 17-project-empty-file-finder/exercises/1-sort-to-a-file/main.go
+++ 17-project-empty-file-finder/exercises/1-sort-to-a-file/main.go
@@ -8,6 +8,13 @@
 
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"sort"
+)
+
 // ---------------------------------------------------------
 // EXERCISE: Sort and write items to a file
 //
@@ -48,4 +55,23 @@ package main
 // ---------------------------------------------------------
 
 func main() {
+	items := os.Args[1:]
+	if len(items) == 0 {
+		fmt.Println("Send me some items and I will sort them")
+		return
+	}
+
+	sort.Strings(items)
+
+	var data []byte
+	for _, s := range items {
+		data = append(data, s...)
+		data = append(data, '\n')
+	}
+
+	err := ioutil.WriteFile("sorted.txt", data, 0644)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
--- 17-project-empty-file-finder/exercises/3-print-directories/main.go
+++ 17-project-empty-file-finder/exercises/3-print-directories/main.go
@@ -8,6 +8,12 @@
 
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
 // ---------------------------------------------------------
 // EXERCISE: Find and write the names of subdirectories to a file
 //
@@ -68,4 +74,38 @@ package main
 // ---------------------------------------------------------
 
 func main() {
+	paths := os.Args[1:]
+	if len(paths) == 0 {
+		fmt.Println("Please provide directory paths.")
+		return
+	}
+
+	var dirs []byte
+
+	for _, dir := range paths {
+		files, err := ioutil.ReadDir(dir)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		dirs = append(dirs, dir...)
+		dirs = append(dirs, '\n')
+
+		for _, file := range files {
+			if file.IsDir() {
+				dirs = append(dirs, '\t')
+				dirs = append(dirs, file.Name()...)
+				dirs = append(dirs, '/', '\n')
+			}
+		}
+
+		dirs = append(dirs, '\n')
+	}
+
+	err := ioutil.WriteFile("dirs.txt", dirs, 0644)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
