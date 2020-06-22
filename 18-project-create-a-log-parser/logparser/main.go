f6ede73b7e99e062565fc6664d4ac35048a873a0
MrRicksan <ricardooliveira.info@gmail.com>
2020-06-22 09:05:04 +0100

    the log parser

---
+++ 18-project-create-a-log-parser/logparser/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	var (
+		sum     map[string]int // Store total visits per unique domain
+		domains []string
+		total   int
+		lines   int
+	)
+
+	sum = make(map[string]int)
+
+	in := bufio.NewScanner(os.Stdin)
+
+	for in.Scan() {
+		lines++
+
+		fields := strings.Fields(in.Text())
+		if len(fields) != 2 {
+			fmt.Printf("wrong input: %v (line #%d)\n", fields, lines)
+			return
+		}
+
+		domain := fields[0]
+
+		visits, err := strconv.Atoi(fields[1])
+		if visits < 0 || err != nil {
+			fmt.Printf("wrong input: %q (line #%d)\n", fields[1], lines)
+			return
+		}
+
+		// Store unique domain names
+		if _, ok := sum[domain]; !ok {
+			domains = append(domains, domain)
+		}
+
+		total += visits
+		sum[domain] += visits
+	}
+
+	fmt.Printf("%-30s %10s\n", "DOMAIN", "VISITS") // Table header
+	fmt.Println(strings.Repeat("-", 45))
+
+	sort.Strings(domains)
+	for _, domain := range domains {
+		visits := sum[domain]
+		fmt.Printf("%-30s %10d\n", domain, visits)
+	}
+	fmt.Printf("\n%-30s %10d\n", "TOTAL", total)
+
+	if err := in.Err(); err != nil {
+		fmt.Println("> Err:", err)
+	}
+}
